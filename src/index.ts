b47fb7cce0b730cd6d62f622effc52832b5a9fb0
anubra266 <anubra266@gmail.com>
2025-09-20 13:37:41 -0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { mount } from 'ripple';
 // @ts-expect-error: known issue, we're working on it
 import { App } from './App.ripple';
+import "./styles.css"
 
 mount(App, {
 	target: document.getElementById('root'),
